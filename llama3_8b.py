8ca3aa9c3f4a589f9f3703a98ec6adabca92265d
Srijan-D <srijandubey3105@gmail.com>
2024-07-07 05:36:41 +0530

    adding llm parameters topk, temperatur for randomness of llama3 llm

--- llama3_8b.py
+++ llama3_8b.py
@@ -9,12 +9,17 @@ app = Flask(__name__, static_url_path='', static_folder='.')
 load_dotenv(find_dotenv())  # Load environment variables
 
 HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+# Get your token from here: https://huggingface.co/settings/tokens and set it in the .env file
 
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
     task="text-generation",
-    max_new_tokens=100,
+    max_new_tokens=300,
     do_sample=False,
+    top_k = 40,
+    top_p = 0.9,
+    temperature = 0.7,
+    huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN
 )
 
 @app.route('/')
@@ -32,6 +37,7 @@ def query():
     try:
         response = llm.invoke(query)
         wrapped_text = textwrap.fill(response, width=100, break_long_words=False, replace_whitespace=False)
+        #  return render_template_string(open('index.html').read(), response=wrapped_text) # This is a bad idea as template can be huge but it is used to pass the response as a variable to the template(html)
         return wrapped_text
     except Exception as e:
         return jsonify({'error': str(e)}), 500
